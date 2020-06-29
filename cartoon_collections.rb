9f3db85a6708ef82df052f8f794805bb66e5e751
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-29 16:01:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index
+  dwarves.each_with_index {|dwarf, index|
+    puts "#{index} #{dwarf}"
 end
 
 def summon_captain_planet# code an argument here
