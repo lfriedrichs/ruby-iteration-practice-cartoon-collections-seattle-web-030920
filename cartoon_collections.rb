738dc67e1973f7e5f23542cf3b2e843c140e6120
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-29 16:07:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-  words.select
+  words.select {|word| word.size > 4}
 end
 
 def find_the_cheese# code an argument here
