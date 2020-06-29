d63d2fb88bdd94806585c308572735d55b313c07
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-29 16:05:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.
 end
 
 def find_the_cheese# code an argument here
